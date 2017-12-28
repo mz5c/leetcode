647371b0ba8fd864b19286892b53d2410b8ecb53
iamrico <w11w23c58@126.com>
2017-12-28 14:10:31 +0800

    N-QueensII.js

--- README.md
+++ README.md
@@ -47,6 +47,7 @@
 |49|[Group Anagrams](https://leetcode.com/problems/group-anagrams/description/) | [JavaScript](./solutions/javascript/GroupAnagrams/GroupAnagrams.js)|Medium|
 |50|[Pow(x, n)](https://leetcode.com/problems/powx-n/description/) | [JavaScript](./solutions/javascript/Powxn/Powxn.js)|Medium|
 |51|[N-Queens](https://leetcode.com/problems/n-queens/description/) | [JavaScript](./solutions/javascript/N-Queens/N-Queens.js)|Hard|
+|52|[N-Queens II](https://leetcode.com/problems/n-queens-ii/description/) | [JavaScript](./solutions/javascript/N-QueensII/N-QueensII.js)|Hard|
 |54|[Spiral Matrix](https://leetcode.com/problems/spiral-matrix/description/) | [JavaScript](./solutions/javascript/SpiralMatrix/SpiralMatrix.js)|Medium|
 |55|[Jump Game](https://leetcode.com/problems/jump-game/description/) | [JavaScript](./solutions/javascript/JumpGame/JumpGame.js)|Medium|
 |57|[Insert Interval](https://leetcode.com/problems/insert-interval/description/) | [JavaScript](./solutions/javascript/InsertInterval/InsertInterval.js)|Hard|
---
+++ solutions/javascript/N-QueensII/N-QueensII.js
@@ -0,0 +1,76 @@
+var res = [];
+var pad_arr = function (n, c) {
+    var pa = [];
+    for (var i = 0; i < n; i++) {
+        pa[i] = c;
+    }
+    return pa;
+};
+var getInitArr = function (n) {
+    var gia = [];
+	for (var i = 0; i < n; i++) {
+        gia[i] = pad_arr(n, '.');
+    }
+    return gia;
+};
+var isValid = function (arr, x, y, n) {
+    var i = x - 1;
+    var j = y - 1;
+	while (i >= 0 && j >= 0) {
+        if (arr[i][j] == 'Q') {
+            return false;
+        }
+        i -= 1;
+        j -= 1;
+    }
+    i = x - 1;
+    j = y;
+    while (i >= 0) {
+        if (arr[i][j] == 'Q') {
+            return false;
+        }
+        i -= 1;
+    }
+    i = x - 1;
+    j = y + 1;
+    while (i >= 0 && j < n) {
+        if (arr[i][j] == 'Q') {
+            return false;
+        }
+        i -= 1;
+        j += 1;
+    }
+    return true;
+};
+var nq = function (arr, idx, n) {
+    arr = arr.slice();
+    if (idx == n) {
+        res++;
+        return;
+    }
+    for (var i = 0; i < n; i++) {
+        arr[idx] = pad_arr(n, '.');
+        arr[idx][i] = 'Q';
+        if (idx == 0) {
+            nq(arr, idx + 1, n);
+        } else if (isValid(arr.slice(), idx, i, n)) {
+            nq(arr, idx + 1, n);
+        }
+    }
+};
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function(n) {
+    res = 0;
+    if (n == 1) {
+        return 1;
+    } else if (n < 4) {
+        return 0;
+    } else {
+        var arr = getInitArr(n);
+        nq(arr.slice(), 0, n);
+    }
+    return res;
+};
