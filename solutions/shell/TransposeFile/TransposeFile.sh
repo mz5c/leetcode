bb20b5c7b7fb9e6c00ceb06bf49b0d709c78fff4
wucheng <w11w23c58@126.com>
2019-08-20 07:07:45 +0800

    modify question-TransposeFile

--- solutions/shell/TransposeFile/TransposeFile.sh
+++ solutions/shell/TransposeFile/TransposeFile.sh
@@ -2,16 +2,16 @@
 # Read from the file file.txt and print its transposed content to stdout.
 awk '
 {
-	    for (i = 1; i <= NF; i++) {
-			        if(NR == 1) {
-						            s[i] = $i;
-									        } else {
-												            s[i] = s[i] " " $i;
-															        }
-																	    }
+    for (i = 1; i <= NF; i++) {
+        if(NR == 1) {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
 }
 END {
-	    for (i = 1; s[i] != ""; i++) {
-			        print s[i];
-					    }
+    for (i in s) {
+        print s[i];
+    }
 }' file.txt
