484cbddd17270f3965aa0afa90fe927568363583
wucheng <w11w23c58@126.com>
2019-09-04 22:24:29 +0800

    modify question-SearchforaRange

---
+++ solutions/java/SearchforaRange/SearchforaRange.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    res[0] = mid;
+                    break;
+                }
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (res[0] == -1) {
+            return res;
+        }
+        left = res[0];
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    res[1] = mid;
+                    break;
+                }
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
---
+++ thinking/SearchforaRange/readme.md
@@ -0,0 +1,3 @@
+### 解题思路
+>原题名：Search for a Range，现题名：Search for a Range  
+>解法：利用2次二分搜索
