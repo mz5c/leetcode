943419985882e41c0f1e6a4a83d0bf215bed7a8e
wucheng.wu <w11w23c58@126.com>
2019-09-12 11:37:39 +0800

    Update HappyNumber.java

--- solutions/java/HappyNumber/HappyNumber.java
+++ solutions/java/HappyNumber/HappyNumber.java
@@ -1,18 +1,18 @@
 class Solution {
-	public boolean isHappy(int n) {
-		int tmp;
-		int sum = 0;
-		if (n == 1 || n == 7) {
-			return true;
-		} else if (n < 10) {
-			return false;
-		} else {
-			while (n != 0) {
-				tmp = n % 10;
-				sum += tmp * tmp;
-				n /= 10;
-			}
-		}
-		return isHappy(sum);
-	}
+    public boolean isHappy(int n) {
+        int tmp;
+        int sum = 0;
+        if (n == 1 || n == 7) {
+            return true;
+        } else if (n < 10) {
+            return false;
+        } else {
+            while (n != 0) {
+                tmp = n % 10;
+                sum += tmp * tmp;
+                n /= 10;
+            }
+        }
+        return isHappy(sum);
+    }
 }
